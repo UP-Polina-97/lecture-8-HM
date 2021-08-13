344d328e816119afa2a41eab54ab5b24d7d02408
Polina <ustinovapolina1@gmail.com>
2021-08-13 07:17:48 +0200

    added some changes to the recipies code

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-#cook_book = {}
-with open('recipes.txt', 'r', encoding='utf-8') as menu:
+from pprint import pprint
+cook_book = {}
+
+def record_cook_book(file):
+  with open(file, 'r', encoding='utf-8') as menu:
     for line in menu:
         ingredients = []
-        cook_book = {}
         recipe_name = line.strip()
         ingredients_amount = int(menu.readline().strip())
         for i in range(ingredients_amount):
-            #menu.readline().strip()
             name, count, mesure = menu.readline().strip().split("|")
-            ingredients.append({'ingredient_name': name.strip(),'quantity': int(count), 'measure': mesure.strip() })
+            ingredients.append({'ingredient_name': name.strip(),'quantity': int(count), 'measure': mesure.strip(),})
         menu.readline()
         cook_book[recipe_name] = ingredients
-        print(cook_book)
 
+  return (cook_book)
+p = record_cook_book('recipes.txt')
+print(p)
+
+print('')
 print('exercise 2:')
+print('')
+
 shop_list = {}
 
 def get_shop_list_by_dishes(dishes, person_count):
-  shop_list = {}
-  for key, value in cook_book.items():
-    if dishes == key:
-      shop_list.add(value)
-      shop_list['quantity'] * person_count
-      print(shop_list)
+    for dish in dishes:
+        for ingredients in cook_book[dish]:
+            shop_list[ingredients['ingredient_name']] = {'measure': ingredients['measure'], 'quantity': int(ingredients['quantity'] * person_count)}
+        [print(key, ':', value) for key, value in shop_list.items()]
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
+list_for_shop = get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)
+print(list_for_shop)
